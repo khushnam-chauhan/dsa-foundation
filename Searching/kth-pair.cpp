a52d4b7c33f28e44bbf797138c6eb26847750a8a
khushnam <chauhankhushnam@gmail.com>
2024-02-05 10:39:07 +0530

    kth pair with binary search

--- Searching/kth-pair.cpp
+++ Searching/kth-pair.cpp
@@ -4,30 +4,69 @@
 #include<algorithm>
 using namespace std;
 
-int kPairs(vector<int> v, int k){
-    int i=0;
-    int j=i+1;
-    set<pair<int,int>> ans;
-    while (j < v.size())
-    {
-        if (abs(v[i]-v[j])==k)
-        {
-            ans.insert({v[i], v[j]});
-            i++;
-            j++;
-        }
-        else if(abs(v[i]-v[j])>k){ 
-            i++;
-        }
-        else{
-            j++;
-        }
+// two pointers approach
+// int kPairs(vector<int> v, int k){
+//     int i=0;
+//     int j=i+1;
+//     set<pair<int,int>> ans;
+//     while (j < v.size())
+//     {
+//         if (abs(v[i]-v[j])==k)
+//         {
+//             ans.insert({v[i], v[j]});
+//             i++;
+//             j++;
+//         }
+//         else if(abs(v[i]-v[j])>k){ 
+//             i++;
+//         }
+//         else{
+//             j++;
+//         }
        
         
         
+//     }
+//     int count=ans.size();
+//     return count;
+    
+
+// }
+
+//binary search approach
+int bs(vector<int> &v,int start,int k ){
+    int end=v.size();
+    while (start<end)
+    {
+        int mid=(start+end)/2;
+        if (v[mid]==k)
+        {
+            return mid;  
+        }
+        else if(k>v[mid]){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+        
     }
-    int count=ans.size();
-    return count;
+    return -1;
+    
+
+}
+int kPairs(vector<int> &v, int k){
+    sort(v.begin(),v.end());
+    set<pair<int,int>> ans;
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (bs(v,i+1,(v[i]+k))!=-1)
+        {
+            ans.insert({v[i],(v[i]+k)});
+        }
+        
+    }
+    return ans.size();
     
 
 }
