626d7197fd51cf5b39f936191a9fbd825a943fc8
khushnam <chauhankhushnam@gmail.com>
2024-04-27 14:37:43 +0530

    char and strings

---
+++ sdegroup/string/string.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<string.h>
+using namespace std;
+
+
+
+int main(){
+    string str;
+    cin>> str; // this take only string before space 
+    //khushnam chauhan o/p- khushnam
+    cout<< str;
+    getline(cin, str); // this take string with space
+    //khushnam chauhan o/p- khushnam chauhan
+    cout<< str;
+    
+    return 0;
+
+}
