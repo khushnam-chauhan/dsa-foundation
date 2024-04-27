b8254536f1aac810fffd1ab36720827502676540
khushnam <chauhankhushnam@gmail.com>
2024-04-27 14:40:09 +0530

    updted files

--- loops/pattern1.cpp
+++ loops/pattern1.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main (){
     for(int i=0;i<5;i++){
-        cout<<""<<endl;
         for(int j=0;j<5;j++){
             cout<<"*";
         }
+        cout<<""<<endl;
     }
 }
