a12eacaae7a7340eba2849ec11fa074b8894801a
khushnam <chauhankhushnam@gmail.com>
2024-07-03 11:41:05 +0530

    string search key

--- sdegroup/recursion/string.cpp
+++ sdegroup/recursion/string.cpp
@@ -3,10 +3,19 @@
 #include<algorithm>
 using namespace std;
 
-int function(){
-
+int checkKey(string& str, int i, int& n, char& key){
+    if(i>=n)
+        return -1;
+    if(str[i]==key)
+        return i;
+    return checkKey(str, i+1, n, key);
 }
 
 int main(){
-
+    string str="khushnam";
+    char key= 'a';
+    int n=str.length();
+    int i=0;
+    int ans= checkKey(str,i,n,key);
+    cout<<ans;
 }
